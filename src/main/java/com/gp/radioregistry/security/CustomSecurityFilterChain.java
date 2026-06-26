dfac7371b4bbc9e18e1e5c710820611e28ef0b9a
giuliopetteno <giulio.petteno@gmail.com>
2026-06-27 00:58:33 +0200

    Tweaks & comments to CustomSecurityFilterChain

--- src/main/java/com/gp/radioregistry/security/CustomSecurityFilterChain.java
+++ src/main/java/com/gp/radioregistry/security/CustomSecurityFilterChain.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -11,6 +12,11 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,7 +25,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class CustomSecurityFilterChain {
    @Bean
    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(CsrfConfigurer::disable)
+        http.cors(Customizer.withDefaults()) // explicitly restricted to http://localhost:4200
+                // CSRF protection is disabled because this application does not use cookies for authentication
+                .csrf(CsrfConfigurer::disable)
+                // Using HTTP protocol only for non-production environments
                 .redirectToHttps(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/roles/**", "/organizations/**", "/compartments/**",
@@ -30,7 +39,8 @@ public class CustomSecurityFilterChain {
         return http.build();
     }
 
-/*    @Bean
+    /*  Disabled, using CustomUserDetailsService
+    @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
     }*/
@@ -43,5 +53,20 @@ public class CustomSecurityFilterChain {
     @Bean
     public CompromisedPasswordChecker compromisedPasswordChecker() {
         return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+
+    // CORS is explicitly restricted to localhost for local development.
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfig.setAllowedMethods(List.of("*"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(List.of("*"));
+        corsConfig.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource corsConfigSource = new UrlBasedCorsConfigurationSource();
+        corsConfigSource.registerCorsConfiguration("/**", corsConfig);
+        return corsConfigSource;
     }
 }
